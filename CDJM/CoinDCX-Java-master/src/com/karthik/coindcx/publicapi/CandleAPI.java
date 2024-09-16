e2bdd67bcb902c8c317c3b343a737784bfbeb13d
Karthik816ml <94064911+theanimenexus@users.noreply.github.com>
2024-09-16 18:47:41 +0530

    Add files via upload

---
+++ CDJM/CoinDCX-Java-master/src/com/karthik/coindcx/publicapi/CandleAPI.java
@@ -0,0 +1,33 @@
+package com.karthik.coindcx.publicapi;
+
+import org.json.JSONException;
+
+import com.karthik.coindcx.httpclient.CoinDCXRequestHandler;
+
+import java.io.IOException;
+import java.util.*;
+
+public class CandleAPI {
+    private static final String HOST = "https://public.coindcx.com";
+    private static final String ENDPOINT = "/market_data/candles";
+
+    /** Retrieves historical data for an instrument.
+     * @param from "yyyy-mm-dd" for fetching candles between days and "yyyy-mm-dd hh:mm:ss" for fetching candles between timestamps.
+     * @param to "yyyy-mm-dd" for fetching candles between days and "yyyy-mm-dd hh:mm:ss" for fetching candles between timestamps.
+     * @param token is instruments token.
+     * @param interval can be minute, day, 3minute, 5minute, 10minute, 15minute, 30minute, 60minute.
+     * @return HistoricalData object which contains list of historical data termed as dataArrayList.
+     * @throws IOException is thrown when there is connection related error.
+     * */
+    public CoinCandle[] getHistoricalData(Date from, Date to, String token, String interval) throws IOException, JSONException {
+        
+        Map<String, Object> params = new HashMap<>();
+        params.put("startTime", from.toInstant().toEpochMilli());
+        params.put("endTime", to.toInstant().toEpochMilli());
+        params.put("pair", token);
+        params.put("interval", interval);
+
+        CoinDCXRequestHandler requestHandler = new CoinDCXRequestHandler();
+        return requestHandler.getRequest(HOST+ENDPOINT, params, CoinCandle[].class);
+    }
+}
---
+++ CDJM/CoinDCX-Java-master/src/com/karthik/coindcx/publicapi/CoinTickerMain.java
@@ -0,0 +1,16 @@
+package com.karthik.coindcx.publicapi;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class CoinTickerMain {
+    public static void main(String[] args) throws InterruptedException {
+        CoinTicker coinTicker = new CoinTicker();
+        coinTicker.connect();
+        coinTicker.subscribe(new HashSet<>() {{add("B-BTC_USDT");}});
+        coinTicker.setTickListener(System.out::println);
+        Thread.sleep(3000);
+        coinTicker.unsubscribe(new ArrayList<>() {{add("B-BTC_USDT");}});
+        coinTicker.destroy();
+    }
+}
